78335b28aace02013af4a6a2ed495a0df152a064
professorwade <schofieldw@centralvirginia.edu>
2024-02-01 09:18:30 -0500

    Added two challenges for Unit 3

---
+++ unit3/PickACardChallenge.java
@@ -0,0 +1,23 @@
+package unit3;
+
+/*
+(Picking a card) Write a program that simulates the action of picking a card from a deck of 52 cards. Your program
+displays the suit and rank of the card. Here are sample runs:
+
+<output>
+The card is 10 of Spades
+<end output>
+
+<output>
+The card is Jack of Clubs
+<end output>
+
+Hint: Consider the cards are random numbers from 0 to 51. Card numbers 0 to 12, 13 to 25, 26 to 38, and 39 to 51
+represent 13 Spades, 13 Hearts, 13 Diamonds, and 13 Clubs, respectively. cardNumber / 13 determines the suit of the
+card and cardNumber % 13 determines the rank of the card. Obtain a random number from 0 to 51 and display its rank
+and suit. (Students can write this program in Chapter 3. Later in Chapter 7, the program can be simplified using arrays.)
+
+ */
+
+public class PickACardChallenge {
+}
